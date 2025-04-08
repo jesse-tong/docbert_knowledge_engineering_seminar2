64b3a5f5a0f60ca6007e0de84787edeb0d23a86e
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2025-04-08 23:46:30 +0700

    Update process bar

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ import pandas as pd
 # Set up the Streamlit app
 def app():
     st.title("Phân tích ngôn từ thù địch, phân biệt sử dụng PhoBERT và LSTM")
-
+    
       # Show loading progress bar
     # Load models
     @st.cache_resource
     def load_models():
-        st.progress(0, "Nạp các mô hình...")
+        loading_model_bar = st.progress(0, "Nạp các mô hình...")
         # Load BERT model
         bert_model, bert_device = load_model_bert()
-        st.progress(50, "Mô hình PhoBERT đã được nạp.")
+        loading_model_bar.progress(50, "Mô hình PhoBERT đã được nạp.")
         # Load LSTM model
         lstm_model, lstm_device = load_model_lstm()
-        st.progress(100, "Mô hình LSTM đã được nạp.")  # Complete loading progress
+        loading_model_bar.progress(100, "Mô hình LSTM đã được nạp.")  # Complete loading progress
+        loading_model_bar.empty()
         return bert_model, bert_device, lstm_model, lstm_device
 
     bert_model, bert_device, lstm_model, lstm_device = load_models()
@@ -30,17 +31,18 @@ def app():
             comments = user_input.splitlines()
 
             # Inference with BERT
-            st.progress(0, "Đang phân tích với PhoBERT...")
+            classification_bar = st.progress(0, "Đang phân tích với PhoBERT...")
             bert_predictions = inference(bert_model, bert_device, comments)
             st.write("BERT Predictions:")
             st.dataframe(pd.DataFrame(bert_predictions))
 
-            st.progress(50, "Đang phân tích với LSTM...")
+            classification_bar.progress(50, "Đang phân tích với LSTM...")
 
             # Inference with LSTM
             lstm_predictions = inference(lstm_model, lstm_device, comments)
             st.write("LSTM Predictions:")
-            st.progress(100, "Phân tích hoàn tất!")
+            classification_bar.progress(100, "Phân tích hoàn tất!")
+            classification_bar.empty()
             st.dataframe(pd.DataFrame(lstm_predictions))
         else:
             st.warning("Hãy nhập một vài bình luận.")
